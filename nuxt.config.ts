2d14f984571fc57bf7d384b5f8dcebe0967f2720
Mulyawan <fr1301@protonmail.com>
2025-12-27 08:43:40 +0700

    migrasi icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,30 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  app: {
-    head: {
-      link: [{ rel: "icon", href: "/favicon.ico" }],
+    compatibilityDate: "2025-07-15",
+    app: {
+        head: {
+            link: [{ rel: "icon", href: "/favicon.ico" }],
+        },
     },
-  },
-  devtools: { enabled: false },
-  css: ["~/assets/css/main.css"],
-  vite: { plugins: [tailwindcss()] },
-  modules: ["@nuxtjs/google-fonts"],
-  googleFonts: {
-    families :{
-      "Poppins": true
-    }
-  }
+    devtools: { enabled: false },
+    css: ["~/assets/css/main.css"],
+    vite: { plugins: [tailwindcss()] },
+    modules: ["@nuxtjs/google-fonts", "@solar-icons/nuxt"],
+    solarIcons: {
+        // prefix for auto-import
+        namePrefix: "solar",
+        autoImport: true,
+        provider: true,
+        // configuration icon
+        color: "currentColor",
+        size: 32,
+        weight: "Linear",
+        mirrored: false,
+    },
+    googleFonts: {
+        families: {
+            Poppins: true,
+        },
+    },
 });
